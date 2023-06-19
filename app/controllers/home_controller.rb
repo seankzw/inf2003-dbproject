6f47752a50762217b259145063f1c7654423395d
seankzw <56796695+seankzw@users.noreply.github.com>
2023-06-19 19:38:59 +0800

    merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,14 @@ class HomeController < ApplicationController
   def index
     if current_user
       if current_user.user?
+        if Patient.where(user_id: current_user.id).empty?
+          session[:logon_name] = "User"
+        else
         patient_record = Patient.where(user_id: current_user.id).first
         @patient_id = patient_record.patient_id
         patient_name = patient_record.fname + " " + patient_record.lname
         session[:logon_name] = patient_name
+        end
       elsif current_user.hospitaladmin?
         session[:logon_name] = "hospitaladmin"
       else
