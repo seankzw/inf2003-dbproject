74167bed407a19a681109eb3653e9d2cf7ffaf6d
seankzw <56796695+seankzw@users.noreply.github.com>
2023-06-22 14:09:34 +0800

    toast session fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,27 +16,41 @@ class Users::SessionsController < Devise::SessionsController
             # if user is sign in but not yet completed profile
             if Patient.where(user_id: current_user.id).empty?
               session[:logon_name] = "User"
+              msg = "Welcome back, " + current_user.email + " !"
+              notice = Hash['msg' => msg , 'type' => 'session']
+              flash[:notice] = notice
             else
             # if user is sign in and have completed profile
             patient_record = Patient.where(user_id: current_user.id).first
             patient_name = patient_record.fname + " " + patient_record.lname
             session[:logon_name] = patient_name
+            msg = "Welcome back, " + patient_name + " !"
+            notice = Hash['msg' => msg , 'type' => 'session']
+            session[:patient_id] = patient_record.patient_id
             end
           elsif current_user.hospitaladmin?
             # logon user is hospital admin
             session[:logon_name] = "hospitaladmin"
+            msg = "Welcome back, hospitaladmin !"
+            notice = Hash['msg' => msg , 'type' => 'session']
           else
             # logon user is admin
             session[:logon_name] = "superadmin"
+            msg = "Welcome back, superadmin !"
+            notice = Hash['msg' => msg , 'type' => 'session']
           end
         end
+        flash[:notice] = notice
       end
    end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super do |resource|
+      notice = Hash['msg' => 'See you again', 'type' => 'session']
+      flash[:notice] = notice
+     end
+   end
 
   # protected
 
