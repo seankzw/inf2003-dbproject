13f9b8101def31f8c893af5be8357b1d03990afb
seankzw <56796695+seankzw@users.noreply.github.com>
2023-07-24 23:16:54 +0800

    host for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.host << "inf2003-dbproject-production.up.railway.app"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded any time
