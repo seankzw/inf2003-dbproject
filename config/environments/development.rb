6ab186367752467453ef1d13de07dfdbd9c1997e
seankzw <56796695+seankzw@users.noreply.github.com>
2023-09-20 09:57:27 +0800

    Update host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #Custom domains
+  config.host << 'seankzw.com'
+  config.host << 'loosiongyu.me'
+
+
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
